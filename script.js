39a455ca02e8ed4344ec5b89016edf7c16b71b0c
barterkamp <84913017+barterkamp@users.noreply.github.com>
2022-03-25 08:39:14 +0100

    Updated script

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 
 const faq = document.querySelectorAll(".faq__content");
 
-faq.forEach((faq) => {
-  faq.addEventListener("click", function () {
-    faq.classList.toggle("active");
+faq.forEach((faqitem) => {
+  // loop through faq and listen for clicks on all faq's.
+  faqitem.addEventListener("click", function () {
+    // on click loop through each faq again and remove active class.
+    faq.forEach((elem) => {
+      elem.classList.remove("active");
+    });
+    // add active class to faq item.
+    faqitem.classList.toggle("active");
   });
 });
